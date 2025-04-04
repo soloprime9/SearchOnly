1244f3ae9f8ab628e484b97d74f23e230b8fd045
soloprime9 <primeindia5750@gmail.com>
2025-04-05 01:44:04 +0530

    Create page.jsx

---
+++ src/app/blog/Anupama 5 April 2025 written update/page.jsx
@@ -0,0 +1,40 @@
+"use client";
+  
+import React, { useState } from "react";
+import MarkdownRenderer from "@/components/MarkdownRenderer"; 
+
+export default function Home() {
+  const [markdown, setMarkdown] = useState(`# Anupama 5 April 2025 Written Update: Mohit Planning against Prem
+
+Hello,, friends welcome to the new written update of Anupama (Rupali Ganguli). where we are going to see in today's episode in Anupama. Anupama and Raghav going to behind an unknown person, anupama says to Raghav that this person will be related to the prem case. here we know that Prem is going to jail and that Anupama and Raghav want to save him.
+
+## Anupama and Raghav
+
+both of them go back to the person in today's episode we say another side where Rohit and Rahi talk to each other about Prem. Rahi says that she does not want to see Prem in jail. Rahi is saying something wrong with that person, who wants Prem sent to jail, but if she knows which person, she will not leave him. 
+
+## Anupama want to save Prem
+Anupama Today episode we see that Anupama and Raghav catch that person and find something important updates. anupama send some information to rahi. Rahi opens and sees the photos where Rahi sees the photos of Mohit with that person. Rahi understands that the rohit is behind all of this game, he wants to send Prem to jail. rahi sees another information about Rohit, where she finds that Rohit plans to love Rahi and more important updates. after this Rahi understands that Rohit is a bad man. Rahi wants to see her eyes on Mohit. Then we see that Rohit is coming to Rahi's room. 
+
+## Rahi thinking about Mohit
+
+next, we see that Anupama and Raghav want to understand the person who is behind the game, all of them using their mind, whereas washundhra says that you all fools, so you can not act like lawyers. Anupama said to Raghav that they want to collect some updates about the prem's case. On where other side we see that Parag is so serious about Prem, that he wants to meet Prem. Parag and Anil go together to meet Prem in jail. where Parag is so nervous about meeting Prem, and he tells Anil to leave him with Prem to talk together. Anil lists and goes. Parag is talking to Prem and saying that Prem will come out of jail, he gives some belief to Prem.
+
+## Anil and Parag going to jail
+
+In the Anupama serial today we see, where Parag is sleeping and he has a dream of Prem, where he is not safe and he is finally going to jail, After seeing this parag is going to wake up quickly and thinking about Prem. 
+
+next, we see that Rahi is talking to Mohit, that he is talking to an unknown person, so she thinks that Mohit is behind all of them, Mohit is aggressively talking to that person. On the other hand, we see that the police are going to jail for taking Prem, where anyone talking about his shirt. Where police are going to be angry about hearing all things about prem shirts. So here is today's episode of Anupama End. 
+
+## Anupama Tomorrow Episode Updates
+
+In Anupama, tomorrow we see that the Anupama wants to save Prem and want to help Rahi. So friend for the latest updates you can connect with us via comments
+
+This blog source from <a href="https://www.hotstar.com/in">Jiohotstar</a> and <a href="https://www.iwmbuzz.com/television/written-updates/anupamaa-written-update-5-april-2025-rahi-suspects-mohits-intentions-doubts-if-he-is-framing-prem/2025/04/04"> iwmbuzz</a>
+`);
+
+  return (
+    <div className="min-h-screen bg-gray-100 p-5">
+      <MarkdownRenderer content={markdown} />
+    </div>
+  );
+}
