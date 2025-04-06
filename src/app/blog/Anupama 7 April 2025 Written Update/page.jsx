301171e2651111697300f4bf1bb240c6d0e578f7
soloprime9 <primeindia5750@gmail.com>
2025-04-07 04:13:58 +0530

    Create page.jsx

---
+++ src/app/blog/Anupama 7 April 2025 Written Update/page.jsx
@@ -0,0 +1,62 @@
+"use client";
+import Head from 'next/head';
+  
+import React, { useState } from "react";
+import MarkdownRenderer from "@/components/MarkdownRenderer"; 
+
+export default function Home() {
+  const [markdown, setMarkdown] = useState(`# Anupama 7 April 2025 Written Update: Anupama Celebrate Victory
+
+In today's episode, we are seeing where all of them are thinking about prem. Because everyone is sad for the prem. All of them know that the prem is not guilty. So Anupama and Raghav here struggling to find the important updates to save prem. 
+
+## Rakhi afraid
+
+Later we see the rakhi, who is afraid to think about Mohit. She is very careful to take care of her eyes on Mohit. Now we see that Rakhi here thinking about love when Mohit comes and asks Rakhi what she thinks. But Rahi is not talking about their thinking, she is thinking about Mohit. 
+
+## Rahi celebrate
+
+That time we see Rahi and Mohit talking to each other, where Anupama calls to Rahi. Rahi attended the call and found updates that Ashish was caught by Anupama and Raghav. About finding this update Rahi is so happy because she knows that Prem will come out of jail. Mohit listens to this update, he is so afraid of him. 
+
+
+## Prem Release
+
+Now we see that Anupama and Raghav waiting for Rahi to celebrate Prem is coming out of jail after Ashish gives all the pieces of information about how he fully planned to take his revenge on Prem. So for all of these Ashish takes full revenge on Prem by sending Prem to jail. Now we see that Prem has come out of jail, where all of them celebrating, and Mohit is so afraid about his secret. If his secret will appear from all of them, Mohit is so afraid, but he is smiling. 
+
+After all of them, Parag meets Prem and both of them are happy to meet each other. Parag going to talk to Ashish and he is so angry with him, why is he doing all of these things, But the police stop Parag and go to the police and jail Ashish after doing all of these things to Prem. 
+
+## RamNavami
+
+Later we see that Anupama and his family celebrate Ram Navami with Prem, who has come from jail. all of them talking to each other, where some people from Shah Parivar were not so happy, but showing they were happy on the release of Prem from jail. 
+
+Some of them ask about this to Mohit, but there are Mohit fears that his secret will come in front of all of them, so all of them do not leave him. But here is Mohit changing the question and Asking Anupama some questions. Here Prem is stopping all of them from asking questions to Mohit.
+
+## Mohit secret
+
+Later we see that Mohit is talking to an unknown person, so Prem and Rahi see Mohit. Prem is asking Mohit, what he is doing here with that person, here is Mohit changing the Questions and Talking to Anupama About Khyati.
+
+For the latest update, you can join us on FondPeace.com dear friends.
+
+
+
+This blog source from <a href="https://www.hotstar.com/in">Jiohotstar</a> and <a href="https://www.iwmbuzz.com/television/written-updates/anupamaa-written-update-5-april-2025-rahi-suspects-mohits-intentions-doubts-if-he-is-framing-prem/2025/04/04"> iwmbuzz</a>
+
+** <a href="https://www.fondpeace.com/blog/Yeh Rishta Kya kehlata hai 6 April 2025 Written Update">Yeh Rishta Kya Kehlata Hai 7 April 2025 Written Update</a>**
+
+**<a href="https://www.fondpeace.com/blog/Anupama 5 April 2025 written update"> Anupama 5 April 2025 Written Update</a>**
+
+`);
+
+  return (
+   <div className="min-h-screen bg-gray-100 p-5">
+
+      <Head>
+          <title>Anupama 7 April 2025 Written Update: Anupama Celebrate Victory</title>
+ 
+</Head>
+    <div>
+      
+      <MarkdownRenderer content={markdown} />
+    </div>
+    </div>
+  );
+}
