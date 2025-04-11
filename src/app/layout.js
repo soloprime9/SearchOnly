fd92eeed7c97ee9439083ad2bd9f7df64522a841
soloprime9 <primeindia5750@gmail.com>
2025-04-11 13:33:40 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,20 +11,21 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-            <script async src="https://www.googletagmanager.com/gtag/js?id=G-X27B1YHY7W"></script>
-            <script>
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-            
-              gtag('config', 'G-X27B1YHY7W');
-            </script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-8WL1Z0DS60"></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-8WL1Z0DS60');
+          `}
+        </script>
       </Head>
       <body className={inter.variable}>{children}</body>
     </html>
   );
 }
-
 
 
 
