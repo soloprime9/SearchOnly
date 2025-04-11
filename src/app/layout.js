9b376f05f4a3d1e4e3421bdab2b84f43ab6629db
soloprime9 <primeindia5750@gmail.com>
2025-04-11 13:28:26 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,24 +6,61 @@ const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
 });
- 
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    <Head>
-          
-            <script async src="https://www.googletagmanager.com/gtag/js?id=G-8WL1Z0DS60"></script>
-            <script>
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-            
-              gtag('config', 'G-8WL1Z0DS60');
-            </script>
-    </Head>
-      
+      <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-8WL1Z0DS60"></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-8WL1Z0DS60');
+          `}
+        </script>
+      </Head>
       <body className={inter.variable}>{children}</body>
     </html>
   );
 }
+
+
+
+
+
+
+
+
+
+// import { Inter } from "next/font/google";
+// import "./globals.css";
+// import Head from 'next/head';
+
+// const inter = Inter({
+//   variable: "--font-inter",
+//   subsets: ["latin"],
+// });
+ 
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//     <Head>
+          
+//             <script async src="https://www.googletagmanager.com/gtag/js?id=G-8WL1Z0DS60"></script>
+//             <script>
+//               window.dataLayer = window.dataLayer || [];
+//               function gtag(){dataLayer.push(arguments);}
+//               gtag('js', new Date());
+            
+//               gtag('config', 'G-8WL1Z0DS60');
+//             </script>
+//     </Head>
+      
+//       <body className={inter.variable}>{children}</body>
+//     </html>
+//   );
+// }
