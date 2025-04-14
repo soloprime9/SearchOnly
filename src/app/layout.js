86a6d650f3cec69ada62200aec43785164425b59
soloprime9 <primeindia5750@gmail.com>
2025-04-15 04:34:50 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,9 +10,6 @@ const inter = Inter({
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        
-      </head>
       <body className={inter.variable}>{children}</body>
     </html>
   );
