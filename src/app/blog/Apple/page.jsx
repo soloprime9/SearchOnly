8a92034c9bfc1e5f52d014d1807966da4d842f62
soloprime9 <primeindia5750@gmail.com>
2025-04-19 13:32:00 +0530

    Update page.jsx

--- src/app/blog/Apple/page.jsx
+++ src/app/blog/Apple/page.jsx
@@ -6,9 +6,8 @@ import MarkdownRenderer from "@/components/MarkdownRenderer";
 
 export default function Home() {
   const [markdown, setMarkdown] = useState(`# Select Faster With This Two-Finger Trick on iPhone
-    
+      
 
-        
   Now we are going to know faster selecting feature in iPhone. You can say it Trick or new feature in the iPhone for users to select fast. 
   What's the need for fa ast selecting feature for you? If you are very habitually tapping on apps like Contacts, Mail, Messages, Voice Memos, Notes, and more in any Apple app, here are methods given in points you can use.
   1. Open your favorite app where you want to select multiple items.
