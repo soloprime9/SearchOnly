81d3ea9e2f9af9db3bb2ffcf1b314c08f65548af
soloprime9 <primeindia5750@gmail.com>
2025-04-19 13:39:22 +0530

    Update page.jsx

--- src/app/Reducer/page.jsx
+++ src/app/Reducer/page.jsx
@@ -4,7 +4,7 @@ import Head from "next/head";
 
 import { Geist, Geist_Mono } from "next/font/google";
 // import "./globals.css";
-
+ 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
