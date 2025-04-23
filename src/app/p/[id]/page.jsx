31f8eab8c9380258440ad74744bce0477d1a6719
soloprime9 <primeindia5750@gmail.com>
2025-04-23 17:18:10 +0530

    Add files via upload

---
+++ src/app/p/[id]/page.jsx
@@ -0,0 +1,159 @@
+"use client";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function Single () {
+
+    const [data, setdata] = useState([]);
+    
+    useEffect(() => {
+        const SinglePost = async() => {
+            const path = window.location.pathname;
+            const id = path.split("/").pop();
+            console.log(id);
+
+            try{
+            const response = await axios.get(`http://localhost:4000/content/post/${id}`);
+
+            setdata(response.data);
+            console.log(response.data);
+            }
+            catch(error){
+                console.log(error);
+            }
+
+        };
+        SinglePost();
+    },[])
+
+    return (
+        <div>
+            <div className=' mt-10  '>
+
+                
+            <div  className='grid grid-cols-[150px_1fr_300px] h-screen '>
+
+
+            {/* Starting of Left Sidebzr */}
+                <div className=' w-full font=bold text-2xl my-30 text-between'>
+                        <h4 className='mx-2 my-4'>Worlds</h4>
+                        <h4 className='mx-2 my-4'>Search</h4>
+                        <h4 className='mx-2 my-4'>Account</h4>
+                        <h4 className='mx-2 my-4'>Setting</h4>
+                        <h4 className='mx-2 my-4'>Privacy</h4>
+                        
+
+                </div>
+
+            {/* Starting of Main Content Area         */}
+                <div className=' border-1 border-gray-300 rounded-md h-screen'>
+                    <div className=' rounded-xl p-2 h-auto '>
+
+                        <div className='flex gap-3 mb-6'>
+                            <img src="https://images.news18.com/ibnlive/uploads/2024/10/apple-iphone-16-pro-review-2024-10-b233e14934d84136a958a7037a4011aa-16x9.jpg?impolicy=website&width=640&height=360" alt="" className='w-10 h-10 rounded-full border-2' />
+
+                            <strong className='pt-2'>Human Cant</strong>
+                            <div className='font-bold text-2xl ml-80'>...</div>
+                        </div>
+                        
+                    <p className='cursor-pointer mb-4'>  {data.content}</p>
+                    
+                    <div className='flex justify-center'>
+                    <img src="https://dictionary.cambridge.org/images/thumb/smartp_noun_002_34391.jpg?version=6.0.48" alt="hello" className='w-auto  h-auto border-1 border-gray-300 rounded-2xl'/>
+                    </div>
+                    
+                    
+                    </div>
+                    
+                    <div className='flex gap-2 justify-around p-2 border-1 border-gray-300 '>
+                    
+                    <p className='cursor-pointer border-2 p-2  px-4 rounded-xl'> like</p>
+                    <p className='cursor-pointer border-2 p-2 rounded-xl px-4'>comment</p>
+                    <p className='cursor-pointer border-2 rounded-xl p-2 px-4'>share</p>
+                    <p className='cursor-pointer border-2 rounded-xl p-2 px-4'>Save</p>
+                    
+                    </div>
+                
+                </div>
+
+
+            {/* Starting of RightSide bar */}
+            
+                <div className=' justify-center text-center border-1 border-gray-300 p-4 mx-4 rounded-md'>
+                
+                <div className='flex  gap-10 mb-6  '>
+                            <div className='flex gap-2'>
+                            <img src="https://images.news18.com/ibnlive/uploads/2024/10/apple-iphone-16-pro-review-2024-10-b233e14934d84136a958a7037a4011aa-16x9.jpg?impolicy=website&width=640&height=360" alt="" className='w-10 h-10 rounded-full border-2' />
+
+                            <strong className='pt-2 truncate  '>Human Cant</strong>
+                            </div>
+
+
+                            <button className='font-bold text-lg p-1 border-2 rounded-xl'>Follow</button>
+
+                            
+                        </div> 
+
+                        <div className='flex  gap-10 mb-6  '>
+                            <div className='flex gap-2'>
+                            <img src="https://images.news18.com/ibnlive/uploads/2024/10/apple-iphone-16-pro-review-2024-10-b233e14934d84136a958a7037a4011aa-16x9.jpg?impolicy=website&width=640&height=360" alt="" className='w-10 h-10 rounded-full border-2' />
+
+                            <strong className='pt-2 truncate  '>Human Cant</strong>
+                            </div>
+
+
+                            <button className='font-bold text-lg p-1 border-2 rounded-xl'>Follow</button>
+
+                            
+                        </div> 
+
+                        <div className='flex  gap-10 mb-6  '>
+                            <div className='flex gap-2'>
+                            <img src="https://images.news18.com/ibnlive/uploads/2024/10/apple-iphone-16-pro-review-2024-10-b233e14934d84136a958a7037a4011aa-16x9.jpg?impolicy=website&width=640&height=360" alt="" className='w-10 h-10 rounded-full border-2' />
+
+                            <strong className='pt-2 truncate  '>Human Cant</strong>
+                            </div>
+
+
+                            <button className='font-bold text-lg p-1 border-2 rounded-xl'>Follow</button>
+
+                            
+                        </div> 
+
+                        <div className='flex  gap-10 mb-6  '>
+                            <div className='flex gap-2'>
+                            <img src="https://images.news18.com/ibnlive/uploads/2024/10/apple-iphone-16-pro-review-2024-10-b233e14934d84136a958a7037a4011aa-16x9.jpg?impolicy=website&width=640&height=360" alt="" className='w-10 h-10 rounded-full border-2' />
+
+                            <strong className='pt-2 truncate  '>Human Cant</strong>
+                            </div>
+
+
+                            <button className='font-bold text-lg p-1 border-2 rounded-xl'>Follow</button>
+
+                            
+                        </div> 
+
+                        <div className='flex  gap-10 mb-6  '>
+                            <div className='flex gap-2'>
+                            <img src="https://images.news18.com/ibnlive/uploads/2024/10/apple-iphone-16-pro-review-2024-10-b233e14934d84136a958a7037a4011aa-16x9.jpg?impolicy=website&width=640&height=360" alt="" className='w-10 h-10 rounded-full border-2' />
+
+                            <strong className='pt-2 truncate  '>Human Cant</strong>
+                            </div>
+
+
+                            <button className='font-bold text-lg p-1 border-2 rounded-xl'>Follow</button>
+
+                            
+                        </div> 
+                </div>
+
+                </div>
+            
+
+            </div>
+        </div>
+    )
+
+}
+
+export default Single;
