1fb0e52137ad3708aa990c439ffa223f780ab008
soloprime9 <primeindia5750@gmail.com>
2025-04-23 17:26:47 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -13,7 +13,7 @@ function Posts () {
 
             const Content = async () => {
             try{
-                const response =  await axios.get('http://localhost:4000/content/get');
+                const response =  await axios.get('https://backend-k.vercel.app/content/get');
                 
                 setData(response.data);
                 console.log("Data",response.data);
