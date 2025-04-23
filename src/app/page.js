3f92a07b5f04d01f20417fefe75811bbb6eed6d4
soloprime9 <primeindia5750@gmail.com>
2025-04-23 18:11:32 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Link from 'next/link';
-import { formatPostTime } from './DateFormate';
+import { formatPostTime } from './components/formatPostTime';
 
 function Posts () {
     const [data, setData] = useState([]);
