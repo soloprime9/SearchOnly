9c5eace1529ecf3d677e9732abd1e55f5ef875e5
soloprime9 <primeindia5750@gmail.com>
2025-04-26 15:08:19 +0530

    Update page.jsx

--- src/app/post/[id]/page.jsx
+++ src/app/post/[id]/page.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Head from 'next/head';
-
+ 
 function Single () {
 
     const [data, setdata] = useState([]);
