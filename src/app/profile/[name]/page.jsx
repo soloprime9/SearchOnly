6d0330d152f25bdf9b6394a620610d74d0affeaf
soloprime9 <primeindia5750@gmail.com>
2025-05-07 12:30:00 +0530

    Update page.jsx

--- src/app/profile/[name]/page.jsx
+++ src/app/profile/[name]/page.jsx
@@ -1,6 +1,6 @@
 'use client';
-import SearchBar from '@/Components/SearchBar';
-import Profile from '@/Components/Profile';
+// import SearchBar from '@/Components/SearchBar';
+import Profile from '@/components/Profile';
 import React from 'react';
 
 
