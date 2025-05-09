63a1fbead5ff9a625dcd1b679af4aac1e699081e
soloprime9 <primeindia5750@gmail.com>
2025-05-10 01:56:57 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 // app/layout.js
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from 'next/head';
 
 const inter = Inter({
   variable: "--font-inter",
@@ -10,6 +11,17 @@ const inter = Inter({
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+    <Head>
+      
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-VTZGD645EG"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-VTZGD645EG');
+        </script>
+    </Head>
     <header className=" m-2 flex justify-evenly">
       <div>
       <h1 className="font-bold"><strong><a href="https://www.fondpeace.com/">Fondpeace</a></strong></h1>
