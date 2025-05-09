4e623b4b4d8f8d0f1d2f3b23aa11a10623f53dea
soloprime9 <primeindia5750@gmail.com>
2025-05-10 02:06:20 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,33 +1,82 @@
 // app/layout.js
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Head from 'next/head';
+import Script from "next/script";
 
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
 });
-  
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    <Head>
-      
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-VTZGD645EG"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-        
-          gtag('config', 'G-VTZGD645EG');
-        </script>
-    </Head>
-    <header className=" m-2 flex justify-evenly">
-      <div>
-      <h1 className="font-bold"><strong><a href="https://www.fondpeace.com/">Fondpeace</a></strong></h1>
-      </div>
-    </header>
-      <body className={inter.variable}>{children}</body>
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-VTZGD645EG"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-VTZGD645EG');
+          `}
+        </Script>
+      </head>
+      <body className={inter.variable}>
+        <header className="m-2 flex justify-evenly">
+          <div>
+            <h1 className="font-bold">
+              <strong>
+                <a href="https://www.fondpeace.com/">Fondpeace</a>
+              </strong>
+            </h1>
+          </div>
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
+
+
+
+
+
+
+
+// // app/layout.js
+// import { Inter } from "next/font/google";
+// import "./globals.css";
+// import Head from 'next/head';
+
+// const inter = Inter({
+//   variable: "--font-inter",
+//   subsets: ["latin"],
+// });
+  
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//     <Head>
+      
+//         <script async src="https://www.googletagmanager.com/gtag/js?id=G-VTZGD645EG"></script>
+//         <script>
+//           window.dataLayer = window.dataLayer || [];
+//           function gtag(){dataLayer.push(arguments);}
+//           gtag('js', new Date());
+        
+//           gtag('config', 'G-VTZGD645EG');
+//         </script>
+//     </Head>
+//     <header className=" m-2 flex justify-evenly">
+//       <div>
+//       <h1 className="font-bold"><strong><a href="https://www.fondpeace.com/">Fondpeace</a></strong></h1>
+//       </div>
+//     </header>
+//       <body className={inter.variable}>{children}</body>
+//     </html>
+//   );
+// }
