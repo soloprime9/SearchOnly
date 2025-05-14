eb9d07e64cf8aaf257e4f22612e3812e03b63282
soloprime9 <primeindia5750@gmail.com>
2025-05-14 11:16:45 +0530

    Update Skeleton.jsx

--- src/components/Skeleton.jsx
+++ src/components/Skeleton.jsx
@@ -4,7 +4,7 @@ const Skeleton = () => {
   const cards = Array(10).fill(null); // You can change the number here to show more/less skeletons
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div className="grid grid-cols-1 gap-2">
       {cards.map((_, index) => (
         <div
           key={index}
