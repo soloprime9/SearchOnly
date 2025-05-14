b6cf2012606a44f30fb184c1092c1dd383f9b1d0
soloprime9 <primeindia5750@gmail.com>
2025-05-15 04:59:57 +0530

    Update StatusBar.jsx

--- src/components/StatusBar.jsx
+++ src/components/StatusBar.jsx
@@ -1,13 +1,23 @@
+'use client';
 import Link from 'next/link';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const MobileTopNavBar = () => {
+  const [randomid, setrandomId] = useState(null);
 
   const ListId = ["6822f740837865db851a8fd9","6822f65b837865db851a8fd6", "6822f5e2837865db851a8fd3", "6822e769a506ae6cbbc9bf52", "6822e715a506ae6cbbc9bf4f", "6822e672a506ae6cbbc9bf4c", "6822e645a506ae6cbbc9bf49", "6810749cb98938fd31f6d35b", "68107476b98938fd31f6d358", "68107451b98938fd31f6d355", "6810742ab98938fd31f6d351", "67b28584f090c59509bdaf10", "67b1875a94bde7d066dfcd05", "67b1849f6a9b31f132629d81", "67b184166a9b31f132629d7e", "67aeddfb00c49d67169a054f", "67ab201887be7041220810d4" ]
 
+
+  useEffect(() => {
   const single = ListId[Math.floor(Math.random() * ListId.length)];
 
-  console.log(single);
+  setrandomId(single);
+  // console.log(single);
+
+  }, []);
+
+
+if(!randomid) return null;
 
   return (
     <div className="fixed bottom-0 left-0 right-0 z-50 md:hidden bg-white border-b border-gray-200 shadow-sm">
@@ -20,14 +30,15 @@ const MobileTopNavBar = () => {
           Home
         </button>
         </Link>
-        <Link href={`/short/${single}`}>
+
+        <Link href={`/short/${randomid}`}>
         <button className="flex flex-col items-center text-sm text-gray-700 hover:text-blue-600">
           <svg className="w-6 h-6 mb-1" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
             <path d="M15 10l4.553 2.276a1 1 0 010 1.448L15 16m-6 0V8m0 0l6 4-6 4z" />
           </svg>
           Video
         </button>
-          </Link>
+        </Link>
 
         <Link href="/search"> 
         <button className="flex flex-col items-center text-sm text-gray-700 hover:text-blue-600">
