49a866d3abdd40324daeb327d2c061056cf3cdd1
soloprime9 <primeindia5750@gmail.com>
2025-05-22 13:42:16 +0530

    Update page.jsx

--- src/app/create/page.jsx
+++ src/app/create/page.jsx
@@ -1,84 +1,103 @@
 "use client";
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React from 'react';
+import CreatePage = "@/components/CreatePage";
+
+function Create (){
+
+    return (
+        <CreatePage />
+    )
+}
+
+export default Create;
 
 
-function Creation (){
 
-    const [prompt, Setprompt] = useState("");
-    const [data, setData] = useState([]);
+
+
+
+
+// "use client";
+// import React, { useEffect, useState } from "react";
+// import axios from "axios";
+
+
+// function Creation (){
+
+//     const [prompt, Setprompt] = useState("");
+//     const [data, setData] = useState([]);
     
 
     
-        const GetContent =  async(e)=> {
-            e.preventDefault();
+//         const GetContent =  async(e)=> {
+//             e.preventDefault();
             
             
-            try {
+//             try {
 
                 
 
-                const response = await axios.get(`https://backendk-z915.onrender.com/content/search?q=${prompt}`)
+//                 const response = await axios.get(`https://backendk-z915.onrender.com/content/search?q=${prompt}`)
 
-                console.log(response.data)
-                setData(response.data);
-            }
-            catch(error){
-                console.log(error);
-            }
+//                 console.log(response.data)
+//                 setData(response.data);
+//             }
+//             catch(error){
+//                 console.log(error);
+//             }
 
          
             
 
-        };
+//         };
 
-        const CopyContent = (text) => {
-            navigator.clipboard.writeText(text);
-            alert("Text Successfully Copied");
-        }
+//         const CopyContent = (text) => {
+//             navigator.clipboard.writeText(text);
+//             alert("Text Successfully Copied");
+//         }
         
 
-    return (
-        <div >
-            <div className="m-2">
+//     return (
+//         <div >
+//             <div className="m-2">
 
             
         
-        <form onSubmit={GetContent} className="p-2 m-10 border-2 rounded-sm border-blue-600 text-lg text-center">
-            <input type="text" value={prompt} onChange={(e) => Setprompt(e.target.value)} className="text-2xl p-3 mb-2 w-full border-2 rounded-md border-blue-200 text-center" />
+//         <form onSubmit={GetContent} className="p-2 m-10 border-2 rounded-sm border-blue-600 text-lg text-center">
+//             <input type="text" value={prompt} onChange={(e) => Setprompt(e.target.value)} className="text-2xl p-3 mb-2 w-full border-2 rounded-md border-blue-200 text-center" />
 
-            <button type="submit" className="border-2 mb-2 border-blue-800 bg-gray-300 rounded-md font-bold text-blue-900 text-2xl p-3 justify-center cursor-pointer">Click Now</button>
-        </form>
+//             <button type="submit" className="border-2 mb-2 border-blue-800 bg-gray-300 rounded-md font-bold text-blue-900 text-2xl p-3 justify-center cursor-pointer">Click Now</button>
+//         </form>
 
         
-            <div className="grid grid-cols-3 gap-4 px-2 pb-20 ">
-                {data.length > 0 ? (
-                    data.map((post, index) => (
-                    <div key={index} className="m-2 p-2 border-1 rounded-2xl w-full  object-cover bg-blue-800 text-white font-bold hover:shadow-xl/40 shadow-blue-900 ring-2 border-orange-200 relative pb-10   hover:shadow-[0px_0px_12px_9px_#742a2a]">
+//             <div className="grid grid-cols-3 gap-4 px-2 pb-20 ">
+//                 {data.length > 0 ? (
+//                     data.map((post, index) => (
+//                     <div key={index} className="m-2 p-2 border-1 rounded-2xl w-full  object-cover bg-blue-800 text-white font-bold hover:shadow-xl/40 shadow-blue-900 ring-2 border-orange-200 relative pb-10   hover:shadow-[0px_0px_12px_9px_#742a2a]">
                         
                         
-                        <p> {post}</p>
+//                         <p> {post}</p>
                         
-                        <div className=" pt-2 ">
-                        <button onClick={() => CopyContent(post)} className=" px-6 py-2 bg-black text-white rounded-lg font-bold transform hover:-translate-y-1 transition duration-400">Copy</button>
-                    </div>
+//                         <div className=" pt-2 ">
+//                         <button onClick={() => CopyContent(post)} className=" px-6 py-2 bg-black text-white rounded-lg font-bold transform hover:-translate-y-1 transition duration-400">Copy</button>
+//                     </div>
                         
-                    </div>
-                    ))
-                ) : (
-                    <div className="">
-                    <p className="font-bold "></p>
-                    </div>
-                    )}
-            </div> 
-            <div>
-                <h1></h1>
-            </div>
+//                     </div>
+//                     ))
+//                 ) : (
+//                     <div className="">
+//                     <p className="font-bold "></p>
+//                     </div>
+//                     )}
+//             </div> 
+//             <div>
+//                 <h1></h1>
+//             </div>
         
-        </div>
-                        </div>
-                    )
+//         </div>
+//                         </div>
+//                     )
 
-}
+// }
 
-export default Creation;
+// export default Creation;
