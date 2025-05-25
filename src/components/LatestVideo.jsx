d01dba209efe91182b18b2646bcb427fdde591d1
soloprime9 <primeindia5750@gmail.com>
2025-05-26 02:19:31 +0530

    Update LatestVideo.jsx

--- src/components/LatestVideo.jsx
+++ src/components/LatestVideo.jsx
@@ -178,7 +178,8 @@ const ReelsFeed = () => {
               ></video>
               <div className="absolute bottom-20 md:bottom-[20vh] left-4 z-10 text-white max-w-[80%]">
                   <p className="font-semibold text-lg mb-1">
-                    <a href={/profile/${singlevid.userId?.username}}>@{singlevid.userId?.username}</a>
+                    <a href={`/profile/${singlevid.userId?.username}`}>@{singlevid.userId?.username}</a>
+
                   </p>
                   <p
                     className={text-sm leading-tight cursor-pointer ${expandedId === singlevid._id ? '' : 'line-clamp-1'}}
