684573c9f9f26981c6413402499cff39be8bd9be
soloprime9 <primeindia5750@gmail.com>
2025-06-10 10:25:51 +0530

    Update page.jsx

--- src/app/joke/page.jsx
+++ src/app/joke/page.jsx
@@ -1,11 +1,19 @@
 import React from "react";
-import PostIdDataWrapper from "@/components/PostIdDataWrapper";
-function Reaction (){
+
+function Reaction() {
     return (
-        <div>
-        <PostIdDataWrapper />
+        <div className="reaction-wrapper">
+            <video
+                src="https://www.facebook.com/reel/24674433478812298/?s=ifu&__cft__[0]=AZVnEXsdlWHhEnzqt_4evWewz3j0vTf2HLW_Ku3B94BHq_WIGbQVuHxcR7rlWDyd0AqYEqg96hvJ1x20V42cfK2cQbYBfn3njWb5wOvS2DguOVKou0u6Y2CUnPgeUfbNNrVd8R2rWd04f6r7DywNgqrRzq0Ht9kISN5CPL_nwGpYqLI0lyUrVT4UmzeSmTg7arxJ84_4pXxw4pu5f36j7iMx"
+                controls
+                width="600"
+                height="400"
+                style={{ backgroundColor: "#000" }} // fallback background
+            >
+                Your browser does not support the video tag.
+            </video>
         </div>
-    )
+    );
 }
 
 export default Reaction;
