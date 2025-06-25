f2ac823844fcc86548437446971dd5473fc6e7e7
soloprime9 <primeindia5750@gmail.com>
2025-06-25 15:55:30 +0530

    Update HomeLatestPosts.jsx

--- src/components/HomeLatestPosts.jsx
+++ src/components/HomeLatestPosts.jsx
@@ -50,7 +50,7 @@ function Posts () {
 
          const handleShare = (post) => {
           const postURL = `${window.location.origin}/post/${post._id}`;
-          const shareText = `${post.content}\non Fondpeace`;
+          const shareText = `${post.content}\non Fondpeace website`;
         
           navigator.clipboard.writeText(shareText)
             .then(() => {
