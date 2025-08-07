fb56be6f740fb639f79560d6886c9fe06e1af233
soloprime9 <primeindia5750@gmail.com>
2025-08-07 06:20:48 +0530

    Update Village.jsx

--- src/components/Village.jsx
+++ src/components/Village.jsx
@@ -20,6 +20,7 @@ export default function Feed() {
 
     if (!storedToken || !storedUserId) {
       alert('Authentication Error: Please login again.');
+      window.location.href="/login";
       
     }
 
