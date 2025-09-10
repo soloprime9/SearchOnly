02d11602d3343d809a85a12daa05df43728253cf
soloprime9 <primeindia5750@gmail.com>
2025-09-10 13:20:39 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -33,6 +33,20 @@ export default function RootLayout({ children }) {
 
 
        
+<script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></script>
+<script>
+  window.OneSignalDeferred = window.OneSignalDeferred || [];
+  OneSignalDeferred.push(async function(OneSignal) {
+    await OneSignal.init({
+      appId: "7155774c-af3a-4dbd-8c0e-ff6ed3bd2090",
+      safari_web_id: "web.onesignal.auto.43666e9c-a8ad-4b1e-8de4-10291bcbdb86",
+      notifyButton: {
+        enable: true,
+      },
+    });
+  });
+</script>
+       
   
       </head>
       <body className={inter.variable}>
