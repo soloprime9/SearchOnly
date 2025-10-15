38490803e019ef4562dd32854bdc1df5bd7dbf75
soloprime9 <primeindia5750@gmail.com>
2025-10-15 21:17:09 +0530

    Update Upload.jsx

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -101,7 +101,7 @@ formData.append("tags", extractedTags.join(","));  // ✅ auto add hashtags as t
       setUploadProgress(0); // Reset progress bar
       // window.location.href = "/upload";
     } catch (error) {
-      setMessage("Internal Server Error. Please try again.");
+      setMessage(error);
       console.log(error);
       setUploadProgress(0); // Reset on error
     }
