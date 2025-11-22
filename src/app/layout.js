99348062e43e7ed435f529e3d014bef1b761dfeb
soloprime9 <primeindia5750@gmail.com>
2025-11-22 02:58:28 -0800

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,19 +14,19 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* Google Analytics */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-VTZGD645EG"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-VTZGD645EG');
-          `}
-        </Script>
+        // {/* Google Analytics */}
+        // <Script
+        //   src="https://www.googletagmanager.com/gtag/js?id=G-VTZGD645EG"
+        //   strategy="afterInteractive"
+        // />
+        // <Script id="google-analytics" strategy="afterInteractive">
+        //   {`
+        //     window.dataLayer = window.dataLayer || [];
+        //     function gtag(){dataLayer.push(arguments);}
+        //     gtag('js', new Date());
+        //     gtag('config', 'G-VTZGD645EG');
+        //   `}
+        // </Script>
 
       </head>
 
