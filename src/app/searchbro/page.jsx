9f5946fcb126c71820ccd3b196ffd420fec58648
soloprime9 <primeindia5750@gmail.com>
2025-11-25 20:16:43 +0530

    Create page.jsx

---
+++ src/app/searchbro/page.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+import SearchFull from "@/components/SearchBox";
+
+export default function SearchPage() {
+  return (
+    <div className="pt-24 p-4 max-w-4xl mx-auto">
+      <SearchFull />
+    </div>
+  );
+}
