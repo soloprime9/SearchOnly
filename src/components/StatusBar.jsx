53fe709790b9bdc0aa0a73b8034fa7fe643f6fcc
soloprime9 <primeindia5750@gmail.com>
2025-11-25 15:53:08 +0530

    Update StatusBar.jsx

--- src/components/StatusBar.jsx
+++ src/components/StatusBar.jsx
@@ -21,7 +21,7 @@ const MobileTopNavBar = () => {
   };
 
   return (
-    <div className="fixed bottom-0 left-0 right-0 z-50 md:hidden bg-white border-t border-gray-300 shadow-md">
+    <div className="fixed bottom-0 left-0 right-0 z-50 md:hidden bg-white border-t border-gray-300 shadow-md pt-2">
       <div className="flex justify-around items-center py-2">
         <Link href="/">
           <a className="flex flex-col items-center text-xs text-gray-700 hover:text-blue-600">
