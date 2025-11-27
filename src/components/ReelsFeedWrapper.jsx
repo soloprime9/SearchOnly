23b232cf6b722c10f874083717501ecec83338ef
soloprime9 <primeindia5750@gmail.com>
2025-11-27 12:37:42 +0530

    Update ReelsFeedWrapper.jsx

--- src/components/ReelsFeedWrapper.jsx
+++ src/components/ReelsFeedWrapper.jsx
@@ -1,9 +1,15 @@
-"use client";
+// src/components/ReelsFeedWrapper.jsx
+'use client'; // यह महत्वपूर्ण है!
 
 import dynamic from "next/dynamic";
+import React from 'react';
 
-const ReelsFeed = dynamic(() => import("./ReelsFeed"), { ssr: false });
+// ReelsFeed को केवल Client Side पर लोड करें (SSR को रोकें)
+const ReelsFeed = dynamic(() => import("./ReelsFeed"), { 
+    ssr: false 
+});
 
 export default function ReelsFeedWrapper(props) {
-  return <ReelsFeed {...props} />;
+    // ReelsFeed को केवल क्लाइंट पर रेंडर किया जाएगा
+    return <ReelsFeed {...props} />;
 }
