63f3ac8ff8c1b768d8e6bfc3242add560f5fade9
soloprime9 <primeindia5750@gmail.com>
2025-12-07 08:23:54 +0530

    Create page.jsx

---
+++ src/app/IntroList/Intro/[slug]/page.jsx
@@ -0,0 +1,15 @@
+import ProductPageView from "@/Introcomponents/SingleProductDetail";
+
+export default async function Page({ params }) {
+  const { slug } = params;
+  console.log("Hello : ", slug);
+
+  const res = await fetch(
+    `https://backendk-z915.onrender.com/product/hello/${slug}`,
+    { cache: "no-store" }
+  );
+
+  const data = await res.json();
+
+  return <ProductPageView data={data} />;
+}
