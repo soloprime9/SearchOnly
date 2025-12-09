a3bc45713ed7ab0d5a2e90fff48dd87bd373a64e
soloprime9 <primeindia5750@gmail.com>
2025-12-09 12:33:11 +0530

    Update page.jsx

--- src/app/IntroList/page.jsx
+++ src/app/IntroList/page.jsx
@@ -115,7 +115,7 @@ export default async function Page() {
         }}
       />
 {/* ===== HEADER ===== */}
-       <header className="w-full top-0 left-0 z-50 bg-white shadow-md">
+       <header className="w-full top-0 left-0 pb-2 z-50 bg-white shadow-md">
   <div className="max-w-7xl mx-auto px-2 sm:px-2 lg:px-4 flex items-center justify-between h-6">
     
     <h1 className="font-extrabold px-2 text-blue-600 text-lg sm:text-xl md:text-2xl">
