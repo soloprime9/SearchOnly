cbb5bbb803789662c941ac9509d473abdb90e4b2
soloprime9 <primeindia5750@gmail.com>
2025-12-18 16:52:35 +0530

    Update VillageClient.jsx

--- src/components/VillageClient.jsx
+++ src/components/VillageClient.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Feed from "./Feed";
+import Feed from "./Village";
 
 export default function VillageClient({ initialPosts }) {
   return <Village initialPosts={initialPosts} />;
