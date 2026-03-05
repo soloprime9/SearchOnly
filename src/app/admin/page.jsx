bfa0424a6e9034d1ee3951a1fcb3fa9a5ff1ec3d
soloprime9 <primeindia5750@gmail.com>
2026-03-05 09:17:28 +0530

    Update API endpoint for fetching analytics data

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -7,7 +7,7 @@ export const dynamic = "force-dynamic";
 async function getInitialData() {
   try {
     const res = await fetch(
-      "https://backend-k.vercel.app/analytics/mango/getall",
+      "https://backendk-z915.onrender.com/analytics/mango/getall",
       { cache: "no-store" }
     );
     return res.json();
