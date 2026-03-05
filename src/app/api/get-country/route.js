0fd2662c51722dd3491f0061b95c4b35ca1d1692
soloprime9 <primeindia5750@gmail.com>
2026-03-05 10:50:05 +0530

    Update route.js

--- src/app/api/get-country/route.js
+++ src/app/api/get-country/route.js
@@ -2,7 +2,9 @@ import { NextResponse } from "next/server";
 
 export async function GET(request) {
   const country =
-    request.headers.get("x-vercel-ip-country") || "Unknown";
+  req.headers["x-vercel-ip-country"] ||
+  req.headers["x-forwarded-for-country"] ||
+  "Unknown";
 
   return NextResponse.json({ country });
 }
