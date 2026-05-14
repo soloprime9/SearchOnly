fe4c39295198575516c19d25fa682450e9813999
soloprime9 <primeindia5750@gmail.com>
2026-05-14 19:47:14 +0530

    Update page.jsx

--- src/app/upload/page.jsx
+++ src/app/upload/page.jsx
@@ -1,11 +1,14 @@
 // src/app/page.jsx
 import Upload from '@/components/Upload';
+import FindFriends from "@/components/FindFriends";
+
 
 export default function HomePage() {
   return (
     <div>
       
       <Upload />
+      <FindFriends />
     </div>
   );
 }
