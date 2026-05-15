4a6b2f32b4facc67cd1222a8c841bfe66574ac18
soloprime9 <primeindia5750@gmail.com>
2026-05-15 17:22:40 +0530

    Create page.jsx

---
+++ src/pwa/page.jsx
@@ -0,0 +1,15 @@
+// src/app/page.jsx
+import Upload from '@/components/Upload';
+import LeftSidebar from "@//components/LeftSidebar"
+import Dashboard from "@/components/Dashboard";
+
+
+export default function HomePage() {
+  return (
+    <div>
+      <LeftSidebar/>
+      <Dashboard />
+      
+    </div>
+  );
+}
