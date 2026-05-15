899d418b61717cc68aaadeb4183acf552e5070a1
soloprime9 <primeindia5750@gmail.com>
2026-05-15 09:34:01 +0530

    Update page.jsx

--- src/app/upload/page.jsx
+++ src/app/upload/page.jsx
@@ -1,12 +1,13 @@
 // src/app/page.jsx
 import Upload from '@/components/Upload';
+import LeftSidebar from "@//components/LeftSidebar"
 import FindFriends from "@/components/FindFriends";
 
 
 export default function HomePage() {
   return (
     <div>
-      
+      <LeftSidebar/>
       <Upload />
       <FindFriends />
     </div>
